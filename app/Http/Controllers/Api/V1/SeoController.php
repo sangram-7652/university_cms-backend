e5f98148b0da167020eacf826073bc2aa59e332d
Sangram Singh Yadav <sangramsingh.dev@gmail.com>
2026-07-01 11:32:09 +0530

    Swap sitemap and robots methods in SeoController

--- app/Http/Controllers/Api/V1/SeoController.php
+++ app/Http/Controllers/Api/V1/SeoController.php
@@ -3,91 +3,148 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\Api\Seo\RobotsResource;
-use App\Http\Resources\Api\Seo\SitemapResource;
+use App\Models\Blog;
+use App\Models\Course;
 use App\Models\RobotsSetting;
 use App\Models\SitemapSetting;
+use App\Models\Specialization;
 
 class SeoController extends Controller
 {
     /**
-     * Sitemap Settings
-     */
-    public function sitemap()
-    {
-
-        $sitemap = SitemapSetting::query()
-
-            ->where(
-
-                'university_id',
-
-                university()->id
-
-            )
-
-            ->first();
-
-
-
-        return response()->json([
-
-            'success' => true,
-
-            'message' => 'Sitemap settings fetched successfully',
-
-            'data' => $sitemap
-
-                ? new SitemapResource(
-
-                    $sitemap
-
-                )
-
-                : null,
-
-        ]);
-    }
-
-
-
-
-    /**
-     * Robots Settings
+     * robots.txt
      */
     public function robots()
     {
+        $setting = RobotsSetting::where(
+            'university_id',
+            university()->id
+        )->first();
 
-        $robots = RobotsSetting::query()
+        if (!$setting || !$setting->enabled) {
+            return response('', 404);
+        }
 
-            ->where(
+        $baseUrl = request()->getSchemeAndHttpHost();
 
-                'university_id',
+        $content = [];
 
-                university()->id
+        $content[] = 'User-agent: ' . ($setting->default_user_agent ?: '*');
+        $content[] = '';
 
-            )
+        foreach ($setting->allow_paths ?? [] as $path) {
+            $content[] = 'Allow: ' . $path;
+        }
 
-            ->first();
+        foreach ($setting->disallow_paths ?? [] as $path) {
+            $content[] = 'Disallow: ' . $path;
+        }
 
+        if ($setting->include_sitemap) {
+            $content[] = '';
+            $content[] = 'Sitemap: ' . $baseUrl . '/sitemap.xml';
+        }
 
+        if (!empty($setting->custom_content)) {
+            $content[] = '';
+            $content[] = trim($setting->custom_content);
+        }
 
-        return response()->json([
+        return response(
+            implode("\n", $content),
+            200,
+            [
+                'Content-Type' => 'text/plain'
+            ]
+        );
+    }
 
-            'success' => true,
+    /**
+     * sitemap.xml
+     */
+    public function sitemap()
+    {
+        $setting = SitemapSetting::where(
+            'university_id',
+            university()->id
+        )->first();
 
-            'message' => 'Robots settings fetched successfully',
+        if (!$setting) {
+            abort(404);
+        }
 
-            'data' => $robots
+        $baseUrl = request()->getSchemeAndHttpHost();
 
-                ? new RobotsResource(
+        $xml = [];
 
-                    $robots
+        $xml[] = '<?xml version="1.0" encoding="UTF-8"?>';
+        $xml[] = '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 
-                )
+        // Home
+        $xml[] = '<url>';
+        $xml[] = '<loc>' . $baseUrl . '</loc>';
+        $xml[] = '<changefreq>' . $setting->change_frequency . '</changefreq>';
+        $xml[] = '<priority>1.0</priority>';
+        $xml[] = '</url>';
 
-                : null,
+        // Courses
+        if ($setting->courses_enabled) {
 
-        ]);
+            Course::where('university_id', university()->id)
+                ->where('status', true)
+                ->get()
+                ->each(function ($course) use (&$xml, $baseUrl, $setting) {
+
+                    $xml[] = '<url>';
+                    $xml[] = '<loc>' . $baseUrl . '/courses/' . $course->slug . '</loc>';
+                    $xml[] = '<changefreq>' . $setting->change_frequency . '</changefreq>';
+                    $xml[] = '<priority>' . $setting->priority . '</priority>';
+                    $xml[] = '</url>';
+                });
+        }
+
+        // Blogs
+        if ($setting->blogs_enabled) {
+
+            Blog::where('university_id', university()->id)
+                ->where('status', true)
+                ->get()
+                ->each(function ($blog) use (&$xml, $baseUrl, $setting) {
+
+                    $xml[] = '<url>';
+                    $xml[] = '<loc>' . $baseUrl . '/blogs/' . $blog->slug . '</loc>';
+                    $xml[] = '<changefreq>' . $setting->change_frequency . '</changefreq>';
+                    $xml[] = '<priority>' . $setting->priority . '</priority>';
+                    $xml[] = '</url>';
+                });
+        }
+
+        // Specializations
+        if ($setting->specializations_enabled) {
+
+            Specialization::where('is_active', true)
+                ->whereHas('course', function ($query) {
+                    $query->where('university_id', university()->id);
+                })
+                ->get()
+                ->each(function ($specialization) use (&$xml, $baseUrl, $setting) {
+
+                    $xml[] = '<url>';
+                    $xml[] = '<loc>' . $baseUrl . '/specializations/' . $specialization->slug . '</loc>';
+                    $xml[] = '<changefreq>' . $setting->change_frequency . '</changefreq>';
+                    $xml[] = '<priority>' . $setting->priority . '</priority>';
+                    $xml[] = '</url>';
+                });
+        }
+
+        $xml[] = '</urlset>';
+
+        return response(
+            implode("\n", $xml),
+            200,
+            [
+                'Content-Type' => 'application/xml'
+            ]
+        );
     }
 }
