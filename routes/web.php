80c11cd078f05338c24259faec1e77ee949298a9
Sangram Singh Yadav <sangramsingh.dev@gmail.com>
2026-07-01 11:20:42 +0530

    Update SeoController namespace in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SeoController;
+use App\Http\Controllers\Api\V1\SeoController;
 
 Route::get('/', function () {
     return response()->json([
